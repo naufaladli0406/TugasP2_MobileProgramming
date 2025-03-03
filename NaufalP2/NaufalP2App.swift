af7235b07d8615b7f534b2f910e30a4710577706
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2025-03-03 16:36:02 +0700

    Initial Commit

---
+++ NaufalP2/NaufalP2App.swift
@@ -0,0 +1,17 @@
+//
+//  NaufalP2App.swift
+//  NaufalP2
+//
+//  Created by Naufal Adli on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NaufalP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NaufalP2Tests/NaufalP2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  NaufalP2Tests.swift
+//  NaufalP2Tests
+//
+//  Created by Naufal Adli on 03/03/25.
+//
+
+import Testing
+
+struct NaufalP2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
